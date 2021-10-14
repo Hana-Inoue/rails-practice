b56901c9c54cd6389660be166d4b27030b053364
Hana-Inoue <0806han.i@gmail.com>
2021-10-14 22:12:08 +0900

    testデータのメールアドレスを変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     name { 'hoge' }
-    email { 'hoge@example.com' }
+    sequence(:email) { |number| "h_inoue2+test-#{number}@ga-tech.co.jp" }
     gender { 1 }
     birthday { '2021-10-14' }
   end
