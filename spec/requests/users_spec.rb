eff45d99365098b0d64505bb8807ea424b9a2bbf
Hana-Inoue <0806han.i@gmail.com>
2021-10-14 20:53:51 +0900

    indexのrequest specを記述

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'indexページの表示に成功すること' do
+      get users_path
+      expect(response).to have_http_status(200)
+    end
   end
 end
