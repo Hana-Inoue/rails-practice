811b0a93128fb51e79b3a8cc90bcdc7439f8e1ae
Hana-Inoue <0806han.i@gmail.com>
2021-10-14 21:18:53 +0900

    Userのmodel specを実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :user do
-    name { "MyString" }
+    name { 'hoge' }
+    email { 'hoge@example.com' }
     gender { 1 }
-    birthday { "2021-10-14" }
+    birthday { '2021-10-14' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # 現在はバリデーションを設定していないため、有効になることのみをテスト
+  it '有効になる' do
+    expect(build(:user)).to be_valid
+  end
 end
