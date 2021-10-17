2ce533a95387f83747833b45ab3477f8b724254c
Hana-Inoue <0806han.i@gmail.com>
2021-10-17 22:32:38 +0900

    showのrequest specを実装

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe "Users", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "GET /show" do
+    let(:user) { create(:user) }
+    it 'showページの表示に成功すること' do
+      get user_path(user)
+      expect(response).to have_http_status(200)
+    end
+  end
 end
