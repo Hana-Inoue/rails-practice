58d85e33f08e38941ad0163b2d4d61c4535ce519
Hana-Inoue <0806han.i@gmail.com>
2021-10-18 21:01:15 +0900

    todoを記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  # TODO: バリデーションを元にテスを実装
   # 現在はバリデーションを設定していないため、有効になることのみをテスト
   it '有効になる' do
     expect(build(:user)).to be_valid
