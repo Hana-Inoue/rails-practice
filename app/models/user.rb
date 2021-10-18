bfe633ce493888afc427b3476f1b29246c81bf86
Hana-Inoue <0806han.i@gmail.com>
2021-10-18 18:04:33 +0900

    gender_in_japaneseの定義場所を変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
     women: 1,
     others: 2
   }
+
+  def gender_in_japanese
+    case gender
+    when 'men'
+      '男性'
+    when 'women'
+      '女性'
+    when 'others'
+      'その他'
+    end
+  end
 end
