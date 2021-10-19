d1f76deb8790bcf9348abb046a2511811c21de9a
Hana-Inoue <0806han.i@gmail.com>
2021-10-19 17:36:04 +0900

    gender_in_japaneseメソッドのテストを実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -61,4 +61,28 @@ RSpec.describe User, type: :model do
       expect(user_without_birthday).not_to be_valid
     end
   end
+
+  describe '#gender_in_japanese' do
+    let(:user) { build(:user, gender: gender) }
+    context 'genderがmenの場合' do
+      let(:gender) { :men }
+      it '男性が返される' do
+        expect(user.gender_in_japanese).to eq '男性'
+      end
+    end
+
+    context 'genderがwomenの場合' do
+      let(:gender) { :women }
+      it '女性が返される' do
+        expect(user.gender_in_japanese).to eq '女性'
+      end
+    end
+
+    context 'genderがothersの場合' do
+      let(:gender) { :others }
+      it 'その他が返される' do
+        expect(user.gender_in_japanese).to eq 'その他'
+      end
+    end
+  end
 end
