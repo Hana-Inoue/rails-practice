5032510fb8fa6b1732074a986479cd59c8d947a6
Hana-Inoue <0806han.i@gmail.com>
2021-10-19 21:47:57 +0900

    request specを実装

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
+  describe "GET /users" do
     it 'indexページの表示に成功すること' do
       get users_path
       expect(response).to have_http_status(200)
     end
   end
 
-  describe "GET /show" do
+  describe "GET /users/:id" do
     let(:user) { create(:user) }
     it 'showページの表示に成功すること' do
       get user_path(user)
