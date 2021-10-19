b698f852a5d7a014f6ef173a0e812e7e5f6c8c74
Hana-Inoue <0806han.i@gmail.com>
2021-10-19 21:14:34 +0900

    User model specをリファクタリング

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,69 +1,73 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context '適切な値が全て入力された場合' do
-    let(:user) { build(:user) }
-    it '有効になる' do
-      expect(user).to be_valid
-    end
-  end
+  let(:user) { build(:user) }
 
-  context 'nameがない場合' do
-    let(:user_without_name) { build(:user, name: '') }
-    it '無効になる' do
-      expect(user_without_name).not_to be_valid
+  describe 'validation' do
+    context '適切な値が全て入力された場合' do
+      it '有効になる' do
+        expect(user).to be_valid
+      end
     end
-  end
 
-  context 'nameが重複している場合' do
-    let(:duplicated_user) { build(:user) }
-    it '無効になる' do
-      create(:user)
-      expect(duplicated_user).not_to be_valid
+    context 'nameがない場合' do
+      it '無効になる' do
+        user.name = ''
+        expect(user).not_to be_valid
+      end
     end
-  end
 
-  context 'emailがない場合' do
-    let(:user_without_email) { build(:user, email: '') }
-    it '無効になる' do
-      expect(user_without_email).not_to be_valid
+    context 'nameが重複している場合' do
+      before { create(:user) }
+      it '無効になる' do
+        expect(user).not_to be_valid
+      end
     end
-  end
 
-  context 'emailが重複している場合' do
-    let(:duplicated_user) { build(:user, email: email) }
-    let(:email) { 'h_inoue2+test-1@ga-tech.co.jp' }
-    it '無効になる' do
-      create(:user, email: email)
-      expect(duplicated_user).not_to be_valid
+    describe 'email' do
+      before { user.email = email }
+      let(:email) { 'h_inoue2+test-1@ga-tech.co.jp' }
+
+      context 'emailがない場合' do
+        let(:email) { '' }
+        it '無効になる' do
+          expect(user).not_to be_valid
+        end
+      end
+
+      context 'emailが重複している場合' do
+        before { create(:user, email: email) }
+        it '無効になる' do
+          expect(user).not_to be_valid
+        end
+      end
+
+      context 'emailの大文字・小文字が異なる場合' do
+        it '無効になる' do
+          create(:user, email: email.upcase)
+          expect(user).not_to be_valid
+        end
+      end
     end
-  end
 
-  context 'emailの大文字・小文字が異なる場合' do
-    let(:user) { build(:user, email: email) }
-    let(:email) { 'H_Inoue2+test-1@ga-tech.co.jp' }
-    it '無効になる' do
-      create(:user, email: email.downcase)
-      expect(user).not_to be_valid
+    context 'genderがない場合' do
+      it '無効になる' do
+        user.gender = ''
+        expect(user).not_to be_valid
+      end
     end
-  end
 
-  context 'genderがない場合' do
-    let(:user_without_gender) { build(:user, gender: '') }
-    it '無効になる' do
-      expect(user_without_gender).not_to be_valid
-    end
-  end
-
-  context 'birthdayがない場合' do
-    let(:user_without_birthday) { build(:user, birthday: '') }
-    it '無効になる' do
-      expect(user_without_birthday).not_to be_valid
+    context 'birthdayがない場合' do
+      it '無効になる' do
+        user.birthday = ''
+        expect(user).not_to be_valid
+      end
     end
   end
 
   describe '#gender_in_japanese' do
-    let(:user) { build(:user, gender: gender) }
+    before { user.gender = gender }
+
     context 'genderがmenの場合' do
       let(:gender) { :men }
       it '男性が返される' do
