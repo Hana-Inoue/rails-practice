0e9319dacd8109023dd228cc323db202dc0a70eb
Hana-Inoue <0806han.i@gmail.com>
2021-10-23 00:27:00 +0900

    データ内容を適切に修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    name { 'hoge' }
+    name { 'user' }
     sequence(:email) { |number| "h_inoue2+test-#{number}@ga-tech.co.jp" }
     gender { :men }
     birthday { '2021-10-14' }
