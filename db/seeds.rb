e7a1fdc67c5ff27498376fef69b9711e66e6b546
Hana-Inoue <0806han.i@gmail.com>
2021-10-23 00:25:26 +0900

    データ内容を適切に修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 5.times do |number|
   User.create!(
-    name: "hoge#{number + 1}",
+    name: "user#{number + 1}",
     email: "h_inoue2+test-#{number + 1}@ga-tech.co.jp",
     gender: rand(0..2),
     birthday: rand(Date.parse('1950-01-01')..Date.parse('2020-12-31'))
