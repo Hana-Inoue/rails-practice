2f434bc659b814622ffb668a5ee615c130bb81a2
Hana-Inoue <0806han.i@gmail.com>
2021-10-22 23:39:03 +0900

    destroyのrequest specを実装

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe 'Users', type: :request do
 
   describe 'POST /users' do
     let(:user_params) { attributes_for(:user) }
+
     context '適切なデータが渡された場合' do
       it 'showページへリダイレクトされる' do
         post users_path, params: { user: user_params }
@@ -42,6 +43,7 @@ RSpec.describe 'Users', type: :request do
 
   describe 'GET /users/:id/edit' do
     let(:user) { create(:user) }
+
     it '200番ステータスが返される' do
       get edit_user_path(user)
       expect(response).to have_http_status(200)
@@ -50,6 +52,7 @@ RSpec.describe 'Users', type: :request do
 
   describe 'GET /users/:id' do
     let(:user) { create(:user) }
+
     it '200番ステータスが返される' do
       get user_path(user)
       expect(response).to have_http_status(200)
@@ -84,6 +87,22 @@ RSpec.describe 'Users', type: :request do
         patch user_path(user), params: { user: user_params }
         expect(response).to have_http_status(200)
       end
+    end
+  end
+
+  describe 'DELETE /users/:id' do
+    let!(:user) { create(:user) }
+
+    it 'indexページへリダイレクトされる' do
+      delete user_path(user)
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to root_path
+    end
+
+    it 'Userが1減る' do
+      expect {
+        delete user_path(user)
+      }.to change(User, :count).by(-1)
     end
   end
 end
