eb70a61d8bc2691ff36bd9b2406c0b60e8121938
Hana-Inoue <0806han.i@gmail.com>
2021-10-27 17:40:43 +0900

    複数ロケールファイルの読み込み

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ module App
   class Application < Rails::Application
     config.load_defaults 6.1
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
