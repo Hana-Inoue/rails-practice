279937045bd0e6fb5986ce8fecad0ee422d25e41
Hana-Inoue <0806han.i@gmail.com>
2021-11-02 21:42:07 +0900

    about_activerecordのrequest specを作成

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe 'StaticPages', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'GET about_activerecordページ' do
+    it '200番ステータスを返す' do
+      get static_pages_about_activerecord_path
+      expect(response).to have_http_status(200)
+    end
+  end
 end
