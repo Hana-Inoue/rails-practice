a52087e369754ff7dd150db392f480ba05198666
Hana-Inoue <0806han.i@gmail.com>
2021-11-04 19:29:33 +0900

    seedsファイルの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,17 @@
     name: "user#{number + 1}",
     email: "h_inoue2+test-#{number + 1}@ga-tech.co.jp",
     gender: rand(0..2),
-    birthday: rand(Date.parse('1950-01-01')..Date.parse('2020-12-31'))
+    birthday: rand(Date.parse('1950-01-01')..Date.parse('2020-12-31')),
+    password: 'testtest',
+    password_confirmation: 'testtest'
   )
 end
+
+User.create!(
+  name: 'a_endo@ga-tech.co.jp',
+  email: 'a_endo@ga-tech.co.jp',
+  gender: 0,
+  birthday: '2020-01-01',
+  password: 'a_endo@ga-tech.co.jp',
+  password_confirmation: 'a_endo@ga-tech.co.jp'
+)
