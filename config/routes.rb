6a42a01fdac389173637dcfb42d7d939a59ef46d
Hana-Inoue <0806han.i@gmail.com>
2021-11-04 22:51:41 +0900

    routesにsessionsのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get 'signup', to: 'users#new'
   get 'static_pages/about_rails_logs'
   get 'static_pages/about_activerecord_logs'
-  get 'signup', to: 'users#new'
   resources :users, except: [:new]
 
   root 'users#index'
