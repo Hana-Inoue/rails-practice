0bc6b624fb79f494e29bcddfcbb4422b334b1853
Hana-Inoue <0806han.i@gmail.com>
2021-11-04 21:18:35 +0900

    factorybotにpasswordを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,7 @@ FactoryBot.define do
     sequence(:email) { |number| "h_inoue2+test-#{number}@ga-tech.co.jp" }
     gender { :men }
     birthday { '2021-10-14' }
+    password { 'testtest' }
+    password_confirmation { 'testtest' }
   end
 end
