955b8ee870d486cb79e49e679c3ae7f72ec6d523
Hana-Inoue <0806han.i@gmail.com>
2021-11-06 22:54:28 +0900

    ログアウト機能の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,15 @@ class SessionsController < ApplicationController
 
     if user && user.registered_password_match_with?(params[:session][:password])
       log_in(user)
-      redirect_to root_path, notice: t('layouts.flash.messages.notice')
+      redirect_to root_path, notice: t('layouts.flash.messages.log_in.success')
     else
-      flash.now[:alert] = t('layouts.flash.messages.alert')
+      flash.now[:alert] = t('layouts.flash.messages.log_in.fail')
       render :new
     end
+  end
+
+  def destroy
+    log_out
+    redirect_to login_path, notice: t('layouts.flash.messages.log_out')
   end
 end
