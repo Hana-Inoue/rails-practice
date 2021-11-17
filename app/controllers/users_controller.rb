5291c49548f66c4b80ebffce15a989ebe6963247
Hana-Inoue <0806han.i@gmail.com>
2021-11-17 18:01:59 +0900

    ユーザー登録後に自動ログインされないよう変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,6 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      log_in(@user)
       redirect_to @user, notice: t('layouts.flash.messages.created_user')
     else
       render :new
