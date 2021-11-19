e8609cf6b7709375d52983db80ec9160240ca820
Hana-Inoue <0806han.i@gmail.com>
2021-11-19 23:05:35 +0900

    Authorizationのシードデータを生成

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,26 +4,8 @@ User.create!(
   gender: 0,
   birthday: '2010-12-31',
   password: 'testtest',
-  password_confirmation: 'testtest',
-  index_action: true,
-  show_action: true,
-  new_action: true,
-  edit_action: true,
-  create_action: true,
-  update_action: true,
-  destroy_action: true
+  password_confirmation: 'testtest'
 )
-
-5.times do |number|
-  User.create!(
-    name: "user#{number + 1}",
-    email: "h_inoue2+test-#{number + 1}@ga-tech.co.jp",
-    gender: rand(0..2),
-    birthday: rand(Date.parse('1950-01-01')..Date.parse('2020-12-31')),
-    password: 'testtest',
-    password_confirmation: 'testtest'
-  )
-end
 
 User.create!(
   name: 'a_endo@ga-tech.co.jp',
@@ -31,12 +13,33 @@ User.create!(
   gender: 0,
   birthday: '2020-01-01',
   password: 'a_endo@ga-tech.co.jp',
-  password_confirmation: 'a_endo@ga-tech.co.jp',
-  index_action: true,
-  show_action: true,
-  new_action: true,
-  edit_action: true,
-  create_action: true,
-  update_action: true,
-  destroy_action: true
+  password_confirmation: 'a_endo@ga-tech.co.jp'
 )
+
+(1..5).each do |number|
+  User.create!(
+    name: "user#{number}",
+    email: "h_inoue2+test-#{number}@ga-tech.co.jp",
+    gender: rand(0..2),
+    birthday: rand(Date.parse('1950-01-01')..Date.parse('2020-12-31')),
+    password: 'testtest',
+    password_confirmation: 'testtest'
+  )
+end
+
+admin = User.find_by(email: 'h_inoue2+test-0@ga-tech.co.jp')
+Authorization.actions.each_key do |key|
+  admin.authorizations.create!(action: key)
+end
+
+endo_san = User.find_by(email: 'a_endo@ga-tech.co.jp')
+Authorization.actions.each_key do |key|
+  endo_san.authorizations.create!(action: key)
+end
+
+users = (1..5).map { |number| User.find_by(email: "h_inoue2+test-#{number}@ga-tech.co.jp") }
+users.each do |user|
+  ['index_action', 'show_action', 'new_action', 'create_action'].each do |action|
+    user.authorizations.create!(action: action)
+  end
+end
