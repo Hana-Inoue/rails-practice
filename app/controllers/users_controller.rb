9f7d04bf5a6d1a28470f693bbe739518a0080406
Hana-Inoue <0806han.i@gmail.com>
2021-11-20 18:47:23 +0900

    users_controllerをリファクタリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,18 @@
 class UsersController < ApplicationController
   def index
-    raise NotAuthorizedError if !current_user.authorizations.find_by(action: 'index_action')
+    check_authorization('index_action')
 
     @users = User.all
   end
 
   def show
-    raise NotAuthorizedError if !current_user.authorizations.find_by(action: 'show_action')
+    check_authorization('show_action')
 
     @user = User.find(params[:id])
   end
 
   def new
-    raise NotAuthorizedError if !current_user.authorizations.find_by(action: 'new_action')
+    check_authorization('new_action')
 
     @user = User.new
   end
@@ -20,16 +20,13 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
 
-    if !current_user.authorizations.find_by(action: 'edit_action') && current_user.id != @user.id
-      raise NotAuthorizedError
-    end
+    check_authorization('edit_action', permit_own_user: true, target_user_id: @user.id)
   end
 
   def create
-    raise NotAuthorizedError if !current_user.authorizations.find_by(action: 'create_action')
+    check_authorization('create_action')
 
     @user = User.new(user_params)
-
     if @user.save
       redirect_to @user, notice: t('layouts.flash.messages.created_user')
     else
@@ -40,9 +37,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
 
-    if !current_user.authorizations.find_by(action: 'update_action') && current_user.id != @user.id
-      raise NotAuthorizedError
-    end
+    check_authorization('update_action', permit_own_user: true, target_user_id: @user.id)
 
     if @user.update(user_params)
       redirect_to @user
@@ -54,9 +49,7 @@ class UsersController < ApplicationController
   def destroy
     user = User.find(params[:id])
 
-    if !current_user.authorizations.find_by(action: 'destroy_action') && current_user.id != user.id
-      raise NotAuthorizedError
-    end
+    check_authorization('destroy_action', permit_own_user: true, target_user_id: user.id)
 
     user.destroy
     redirect_to root_path
@@ -68,5 +61,16 @@ class UsersController < ApplicationController
     params
       .require(:user)
       .permit(:name, :email, :gender, :birthday, :password, :password_confirmation)
+  end
+
+  def check_authorization(action, permit_own_user: false, target_user_id: nil)
+    unless permit_own_user
+      raise NotAuthorizedError if !current_user.authorizations.find_by(action: action)
+      return
+    end
+
+    if !current_user.authorizations.find_by(action: action) && current_user.id != target_user_id
+      raise NotAuthorizedError
+    end
   end
 end
