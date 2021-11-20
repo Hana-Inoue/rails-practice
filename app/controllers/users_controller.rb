b1e2c574b8c1ab2e28f960282bd1d21eab42e097
Hana-Inoue <0806han.i@gmail.com>
2021-11-20 16:56:43 +0900

    権限の有無を確認する処理を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,33 @@
 class UsersController < ApplicationController
   def index
+    raise NotAuthorizedError if !current_user.authorizations.find_by(action: 'index_action')
+
     @users = User.all
   end
 
   def show
+    raise NotAuthorizedError if !current_user.authorizations.find_by(action: 'show_action')
+
     @user = User.find(params[:id])
   end
 
   def new
+    raise NotAuthorizedError if !current_user.authorizations.find_by(action: 'new_action')
+
     @user = User.new
   end
 
   def edit
     @user = User.find(params[:id])
+
+    if !current_user.authorizations.find_by(action: 'edit_action') && current_user.id != @user.id
+      raise NotAuthorizedError
+    end
   end
 
   def create
+    raise NotAuthorizedError if !current_user.authorizations.find_by(action: 'create_action')
+
     @user = User.new(user_params)
 
     if @user.save
@@ -28,6 +40,10 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
 
+    if !current_user.authorizations.find_by(action: 'update_action') && current_user.id != @user.id
+      raise NotAuthorizedError
+    end
+
     if @user.update(user_params)
       redirect_to @user
     else
@@ -36,7 +52,13 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
+    user = User.find(params[:id])
+
+    if !current_user.authorizations.find_by(action: 'destroy_action') && current_user.id != user.id
+      raise NotAuthorizedError
+    end
+
+    user.destroy
     redirect_to root_path
   end
 
