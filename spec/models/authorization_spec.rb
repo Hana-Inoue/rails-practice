d8e8ba1803aaaedc5699730be87b8fe2467f1838
Hana-Inoue <0806han.i@gmail.com>
2021-11-20 15:37:27 +0900

    authorization model specを作成

---
+++ spec/models/authorization_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Authorization, type: :model do
+  let(:authorization) { create(:authorization) }
+
+    context '値が全て適切な場合' do
+      it '有効になる' do
+        expect(authorization).to be_valid
+      end
+    end
+
+    context 'user_idが空文字の場合' do
+      before { authorization.user_id = '' }
+      it '無効になる' do
+        expect(authorization).not_to be_valid
+      end
+    end
+
+    context 'user_idがusersテーブルのidカラムに存在しない値の場合' do
+      before { authorization.user_id = User.last.id + 1 }
+      it '無効になる' do
+        expect(authorization).not_to be_valid
+      end
+    end
+
+    context 'actionが空文字の場合' do
+      before { authorization.action = '' }
+      it '無効になる' do
+        expect(authorization).not_to be_valid
+      end
+    end
+end
