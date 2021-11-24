78fcf977a35fe107e45abcf43adde222bc049ccd
Hana-Inoue <0806han.i@gmail.com>
2021-11-24 23:22:50 +0900

    action modelにバリデーションを設定

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,4 +1,7 @@
 class Action < ApplicationRecord
   has_many :authorizations, dependent: :destroy
   has_many :users, through: :authorizations, dependent: :destroy
+
+  validates :controller, presence: true
+  validates :action, presence: true
 end
