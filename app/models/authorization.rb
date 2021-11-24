43265f85d335b97bab8908a7dc83ca8454761076
Hana-Inoue <0806han.i@gmail.com>
2021-11-24 17:59:43 +0900

    テーブル変更に伴うリレーションの追加とバリデーションの変更

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -1,16 +1,7 @@
 class Authorization < ApplicationRecord
   belongs_to :user
-
-  enum action: {
-    index_action: 0,
-    show_action: 1,
-    new_action: 2,
-    edit_action: 3,
-    create_action: 4,
-    update_action: 5,
-    destroy_action: 6
-  }
+  belongs_to :action
 
   validates :user_id, presence: true
-  validates :action, inclusion: { in: actions.keys }, presence: true
+  validates :action_id, presence: true
 end
