ba3675b211be09122660a229998e07a5bc6d45f1
Hana-Inoue <0806han.i@gmail.com>
2021-11-25 21:23:19 +0900

    test_user traitを作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,11 +7,24 @@ FactoryBot.define do
     password { 'testtest' }
     password_confirmation { 'testtest' }
 
+    ['index', 'show', 'new', 'edit', 'create', 'update', 'destroy'].each do |action|
+      Action.create!(controller: 'user', action: action)
+    end
+
+    # indexの権限のみを所有するtest userの作成
+    trait :test_user do
+      after(:create) do |user|
+        user.user_authorizations <<
+          build(
+            :user_authorization,
+            action_id: Action.find_by(controller: 'user', action: 'index').id
+          )
+      end
+    end
+
+    # 全ての権限を所有するadmin userの作成
     trait :admin do
       after(:create) do |user|
-        ['index', 'show', 'new', 'edit', 'create', 'update', 'destroy'].each do |action|
-          Action.create!(controller: 'user', action: action)
-        end
         Action.all.each do |action|
           user.user_authorizations << build(:user_authorization, action_id: action.id)
         end
