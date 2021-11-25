90a5f315075badf3a892419eb4f8dd567ea8764d
Hana-Inoue <0806han.i@gmail.com>
2021-11-25 15:09:31 +0900

    user_authorization model specが通るよう修正

--- spec/factories/user_authorizations.rb
+++ spec/factories/user_authorizations.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :authorization do
+  factory :user_authorization do
     action_id { 1 }
   end
 end
--- spec/models/user_authorization_spec.rb
+++ spec/models/user_authorization_spec.rb
@@ -1,42 +1,42 @@
 require 'rails_helper'
 
-RSpec.describe Authorization, type: :model do
+RSpec.describe UserAuthorization, type: :model do
   let(:user) { create(:user) }
   let(:action) { create(:action) }
-  let(:authorization) { create(:authorization, user: user, action: action) }
+  let(:user_authorization) { create(:user_authorization, user: user, action: action) }
 
   describe 'validation' do
     context '値が全て適切な場合' do
       it '有効になる' do
-        expect(authorization).to be_valid
+        expect(user_authorization).to be_valid
       end
     end
 
     context 'user_idが空文字の場合' do
-      before { authorization.user_id = '' }
+      before { user_authorization.user_id = '' }
       it '無効になる' do
-        expect(authorization).not_to be_valid
+        expect(user_authorization).not_to be_valid
       end
     end
 
     context 'user_idがusersテーブルのidカラムに存在しない値の場合' do
-      before { authorization.user_id = User.last.id + 1 }
+      before { user_authorization.user_id = User.last.id + 1 }
       it '無効になる' do
-        expect(authorization).not_to be_valid
+        expect(user_authorization).not_to be_valid
       end
     end
 
     context 'action_idが空文字の場合' do
-      before { authorization.action_id = '' }
+      before { user_authorization.action_id = '' }
       it '無効になる' do
-        expect(authorization).not_to be_valid
+        expect(user_authorization).not_to be_valid
       end
     end
 
     context 'action_idがactionsテーブルのidカラムに存在しない値の場合' do
-      before { authorization.action_id = Action.last.id + 1 }
+      before { user_authorization.action_id = Action.last.id + 1 }
       it '無効になる' do
-        expect(authorization).not_to be_valid
+        expect(user_authorization).not_to be_valid
       end
     end
   end
