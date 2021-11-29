ff91d56402d4d6aaeb6493839d766d4afa463026
Hana-Inoue <0806han.i@gmail.com>
2021-11-29 22:08:46 +0900

    user_not_authorizedメソッドのテストを記述

--- spec/requests/application_spec.rb
+++ spec/requests/application_spec.rb
@@ -8,4 +8,18 @@ RSpec.describe 'Application', type: :request do
       expect(response).to redirect_to login_path
     end
   end
+
+  describe '#user_not_authorized' do
+    before { post login_path, params: params }
+    let(:params) { { session: { email: email, password: password } } }
+    let(:email) { user.email }
+    let(:password) { attributes_for(:user)[:password] }
+    let(:user) { create(:user, :user_with_index_authorization) }
+
+    it 'リダイレクトされる' do
+      get user_path(user)
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to root_path
+    end
+  end
 end
