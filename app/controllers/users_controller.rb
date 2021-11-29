7474d4af7a72d77bc8998a830c70f7c9b54142c8
Hana-Inoue <0806han.i@gmail.com>
2021-11-29 21:11:53 +0900

    check_authorizationメソッドの呼び出し元を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,25 @@
 class UsersController < ApplicationController
-  def index
-    check_authorization
+  before_action :check_authorization
 
+  def index
     @users = User.all
   end
 
   def show
-    check_authorization
-
     @user = User.find(params[:id])
   end
 
   def new
-    check_authorization
-
     @user = User.new
   end
 
   def edit
     @user = User.find(params[:id])
-
-    check_authorization(authorize_self_resource: true, resource_owner_id: @user.id)
   end
 
   def create
-    check_authorization
-
     @user = User.new(user_params)
+
     if @user.save
       redirect_to @user, notice: t('layouts.flash.messages.created_user')
     else
@@ -37,8 +30,6 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
 
-    check_authorization(authorize_self_resource: true, resource_owner_id: @user.id)
-
     if @user.update(user_params)
       redirect_to @user
     else
@@ -48,8 +39,6 @@ class UsersController < ApplicationController
 
   def destroy
     user = User.find(params[:id])
-
-    check_authorization(authorize_self_resource: true, resource_owner_id: user.id)
 
     user.destroy
     redirect_to root_path
