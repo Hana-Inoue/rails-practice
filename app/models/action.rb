a8435162c7da35b03b9d3c083fadf886f11dcf09
Hana-Inoue <0806han.i@gmail.com>
2021-11-29 18:50:28 +0900

    model名の変更により旧モデルの削除

--- app/models/action.rb
+++
@@ -1,7 +0,0 @@
-class Action < ApplicationRecord
-  has_many :user_authorizations
-  has_many :users, through: :user_authorizations, dependent: :destroy
-
-  validates :controller, presence: true
-  validates :action, presence: true
-end
