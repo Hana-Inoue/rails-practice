8b6e8bc7a2438eb38dce2f51745a8025670f3974
Hana-Inoue <0806han.i@gmail.com>
2021-11-29 20:26:13 +0900

    テストが通るよう修正

--- spec/models/user_authorization_spec.rb
+++ spec/models/user_authorization_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe UserAuthorization, type: :model do
   let(:user) { create(:user) }
-  let(:action) { create(:action) }
-  let(:user_authorization) { create(:user_authorization, user: user, action: action) }
+  let(:controller_action) { create(:controller_action) }
+  let(:user_authorization) do
+    create(:user_authorization, user: user, controller_action: controller_action)
+  end
 
   describe 'validation' do
     context '値が全て適切な場合' do
@@ -26,15 +28,15 @@ RSpec.describe UserAuthorization, type: :model do
       end
     end
 
-    context 'action_idが空文字の場合' do
-      before { user_authorization.action_id = '' }
+    context 'controller_action_idが空文字の場合' do
+      before { user_authorization.controller_action_id = '' }
       it '無効になる' do
         expect(user_authorization).not_to be_valid
       end
     end
 
-    context 'action_idがactionsテーブルのidカラムに存在しない値の場合' do
-      before { user_authorization.action_id = Action.last.id + 1 }
+    context 'controller_action_idがcontroller_actionsテーブルのidカラムに存在しない値の場合' do
+      before { user_authorization.controller_action_id = ControllerAction.last.id + 1 }
       it '無効になる' do
         expect(user_authorization).not_to be_valid
       end
