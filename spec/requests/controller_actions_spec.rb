537099ecb1e48c5967c8a4eb5a4d9c5baf441741
Hana-Inoue <0806han.i@gmail.com>
2021-12-09 00:31:10 +0900

    controller_actions の update アクションのテストを記述

--- spec/requests/controller_actions_spec.rb
+++ spec/requests/controller_actions_spec.rb
@@ -26,4 +26,26 @@ RSpec.describe "ControllerActions", type: :request do
       end
     end
   end
+
+  describe 'PATCH アクセス権限' do
+    before do
+      patch user_controller_actions_path(other_user), params: { user: controller_action_ids }
+    end
+    let(:controller_action_ids) { { controller_action_ids: ["#{ControllerAction.first.id}"] } }
+
+    context '権限を持つユーザがアクセス権限の変更に成功した場合' do
+      it '302番ステータスを返す' do
+        expect(response).to have_http_status(302)
+      end
+    end
+
+    context '権限を持たないユーザがアクセスしようとした場合' do
+      let(:user) { create(:user, :user_with_index_authorization) }
+
+      it 'ログインページへリダイレクトする' do
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to login_path
+      end
+    end
+  end
 end
