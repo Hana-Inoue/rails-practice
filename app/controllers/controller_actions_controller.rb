5f50ef175d5c92b0e71531b72e036603d98d1f85
Hana-Inoue <0806han.i@gmail.com>
2021-12-08 17:23:04 +0900

    update アクションのリファクタリングとリダイレクト処理の実装

--- app/controllers/controller_actions_controller.rb
+++ app/controllers/controller_actions_controller.rb
@@ -9,24 +9,30 @@ class ControllerActionsController < ApplicationController
     old_controller_action_ids = @user.user_authorizations.pluck(:controller_action_id)
     new_controller_action_ids = controller_action_params[:controller_action_ids].map(&:to_i)
 
-    (new_controller_action_ids - old_controller_action_ids).each do |controller_action_id|
-      ControllerAction.find_by(id: controller_action_id).users << @user
+    if add_user_authorizations(new_controller_action_ids - old_controller_action_ids).all? &&
+        delete_user_authorizations(old_controller_action_ids - new_controller_action_ids).all?
+      redirect_to @user, notice: t('layouts.flash.messages.change_user_authorizations.success')
+    else
+      flash.now[:alert] = t('layouts.flash.messages.change_user_authorizations.fail')
+      render :edit
     end
-    (old_controller_action_ids - new_controller_action_ids).each do |controller_action_id|
-      ControllerAction.find_by(id: controller_action_id).users.delete(@user)
-    end
-
-    # TODO: 修正
-    #if @user.update(controller_action_params)
-    #  redirect_to @user
-    #else
-    #  render :edit
-    #end
   end
 
   private
 
   def controller_action_params
     params.require(:user).permit(controller_action_ids: [])
+  end
+
+  def add_user_authorizations(add_controller_action_ids)
+    add_controller_action_ids.each do |add_controller_action_id|
+      ControllerAction.find_by(id: add_controller_action_id).users << @user
+    end
+  end
+
+  def delete_user_authorizations(delete_controller_action_ids)
+    delete_controller_action_ids.each do |delete_controller_action_id|
+      ControllerAction.find_by(id: delete_controller_action_id).users.delete(@user)
+    end
   end
 end
