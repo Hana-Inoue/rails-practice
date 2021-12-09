1f831fba4df429223586b6ec7e457328502c5b59
Hana-Inoue <0806han.i@gmail.com>
2021-12-09 22:48:18 +0900

    user_authorizations factory の修正に伴い rspec が通るよう修正

--- spec/factories/controller_actions.rb
+++ spec/factories/controller_actions.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :controller_action do
-    controller { 'user' }
-    action { 'index' }
+    controller { UsersController.name.delete_suffix('Controller').underscore }
+    action { UsersController.instance_methods(false).first }
   end
 end
