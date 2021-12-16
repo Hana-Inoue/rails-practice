4f2a65fadcbec49a0f80e9c2eac39c55e99c7a2b
Hana-Inoue <0806han.i@gmail.com>
2021-12-16 20:33:24 +0900

    controller_actions request specをauthorizations request specに修正

--- spec/requests/authorizations_spec.rb
+++ spec/requests/authorizations_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "ControllerActions", type: :request do
+RSpec.describe "Authorizations", type: :request do
   before do
-    create_controller_actions
+    create_authorizations
     log_in(user)
   end
   let(:user) { create(:user, :admin) }
@@ -10,22 +10,22 @@ RSpec.describe "ControllerActions", type: :request do
 
   describe 'GET editページ' do
     it '200番ステータスを返す' do
-      get edit_user_controller_actions_path(other_user)
+      get edit_user_authorizations_path(other_user)
       expect(response).to have_http_status(200)
     end
   end
 
   describe 'PATCH アクセス権限' do
-    let(:controller_action_ids) { { controller_action_ids: ["#{ControllerAction.first.id}"] } }
+    let(:authorization_ids) { { authorization_ids: ["#{ControllerAction.first.id}"] } }
 
     it '302番ステータスを返す' do
-      patch user_controller_actions_path(other_user), params: { user: controller_action_ids }
+      patch user_authorizations_path(other_user), params: { user: authorization_ids }
       expect(response).to have_http_status(302)
     end
 
     it '任意のUserに紐づくuser_authorizationsを変更' do
       expect {
-        patch user_controller_actions_path(other_user), params: { user: controller_action_ids }
+        patch user_authorizations_path(other_user), params: { user: authorization_ids }
       }.to change(other_user.user_authorizations, :count).by(1)
     end
   end
