23d2199239655f016002ff2bc3f234b471beab24
Hana-Inoue <0806han.i@gmail.com>
2021-12-16 20:35:21 +0900

    controller_action model specをauthorization specに修正

--- spec/models/authorization_spec.rb
+++ spec/models/authorization_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe ControllerAction, type: :model do
-  let(:controller_action) { build(:controller_action) }
+RSpec.describe Authorization, type: :model do
+  let(:authorization) { build(:authorization) }
 
   describe 'validation' do
     context '適切な値が全て入力された場合' do
       it '有効になる' do
-        expect(controller_action).to be_valid
+        expect(authorization).to be_valid
       end
     end
 
     context 'controllerが空文字の場合' do
-      before { controller_action.controller = '' }
+      before { authorization.controller = '' }
       it '無効になる' do
-        expect(controller_action).not_to be_valid
+        expect(authorization).not_to be_valid
       end
     end
 
     context 'actionが空文字の場合' do
-      before { controller_action.action = '' }
+      before { authorization.action = '' }
       it '無効になる' do
-        expect(controller_action).not_to be_valid
+        expect(authorization).not_to be_valid
       end
     end
   end
