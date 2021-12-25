5d597bb37707a8b431215f8cc7a62df15c1a59fa
Hana-Inoue <0806han.i@gmail.com>
2021-12-25 22:30:41 +0900

    user_addresses コントローラの修正

--- app/controllers/user_addresses_controller.rb
+++ app/controllers/user_addresses_controller.rb
@@ -1,15 +1,5 @@
 class UserAddressesController < ApplicationController
-  def show
-    @user = User.find_by(id: params[:user_id])
-  end
-
-  def new
-  end
-
   def edit
-  end
-
-  def create
   end
 
   def update
