b5dd845bd030bef7aaa9fec7f30727c9b67ebe17
Hana-Inoue <0806han.i@gmail.com>
2021-12-25 12:19:08 +0900

    UserAddress モデルにバリデーションを定義

--- app/models/user_address.rb
+++ app/models/user_address.rb
@@ -1,3 +1,10 @@
 class UserAddress < ApplicationRecord
   belongs_to :user
+
+  VALID_POSTAL_CODE_REGEX = /\d{3}-\d{4}/
+
+  validates :postal_code, presence: true, format: { with: VALID_POSTAL_CODE_REGEX }
+  validates :prefecture, presence: true, length: { maximum: 20 }
+  validates :city, presence: true, length: { maximum: 50 }
+  validates :address_line, presence: true, length: { maximum: 200 }
 end
