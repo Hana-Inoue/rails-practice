9c1293a15ef2737ba3bd4f8a62839cad03fd1036
Hana-Inoue <0806han.i@gmail.com>
2021-12-26 00:28:33 +0900

    user_addresses edit アクションを修正

--- app/controllers/user_addresses_controller.rb
+++ app/controllers/user_addresses_controller.rb
@@ -1,5 +1,7 @@
 class UserAddressesController < ApplicationController
   def edit
+    @user = User.find_by(id: params[:user_id])
+    @user_address = UserAddress.find_by(user_id: params[:user_id]) || @user.build_user_address
   end
 
   def update
