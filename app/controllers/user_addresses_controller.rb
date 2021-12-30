c95331adf9b85d57bc76f05c4019bfc6b607e5b6
Hana-Inoue <0806han.i@gmail.com>
2021-12-30 14:04:14 +0900

    user_addresses update アクションを実装

--- app/controllers/user_addresses_controller.rb
+++ app/controllers/user_addresses_controller.rb
@@ -5,8 +5,24 @@ class UserAddressesController < ApplicationController
   end
 
   def update
+    @user = User.find_by(id: params[:user_id])
+    @user_address = UserAddress.find_by(user_id: params[:user_id]) || @user.build_user_address
+
+    if @user_address.update(user_address_params)
+      redirect_to @user
+    else
+      render :edit
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def user_address_params
+    params
+      .require(:user_address)
+      .permit(:postal_code, :prefecture, :city, :address_line)
   end
 end
