79abadc081bb86303c4b1b1ed7a112fa3fd81b4e
Hana-Inoue <0806han.i@gmail.com>
2021-12-30 14:55:43 +0900

    user_addresses update request spec を作成

--- spec/requests/user_addresses_spec.rb
+++ spec/requests/user_addresses_spec.rb
@@ -13,4 +13,39 @@ RSpec.describe "UserAddresses", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'PATCH 住所情報' do
+    before do
+      create(:user_address, user: user)
+      user_address_params[:postal_code] = postal_code
+    end
+    let(:user_address_params) { attributes_for(:user_address) }
+
+    context '有効なリクエストパラメータが渡された場合' do
+      let(:postal_code) { '111-1111' }
+
+      it 'showページへリダイレクトする' do
+        patch user_user_address_path(user), params: { user_address: user_address_params }
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to user_path(User.last)
+      end
+
+      it '任意のUserの値を変更する' do
+        expect {
+          patch user_user_address_path(user), params: { user_address: user_address_params }
+        }.to change { UserAddress.find_by(user_id: user.id)[:postal_code] }
+          .from('000-0000')
+          .to(postal_code)
+      end
+    end
+
+    context '無効なリクエストパラメータが渡された場合' do
+      let(:postal_code) { '1111111' }
+
+      it '200番ステータスを返す' do
+        patch user_user_address_path(user), params: { user_address: user_address_params }
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
 end
