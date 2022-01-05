526e955887e1cea403b1bf02aea1de815553eef3
Hana-Inoue <0806han.i@gmail.com>
2022-01-05 13:42:27 +0900

    UserPost モデルのアソシエーションを修正

--- app/models/user_post.rb
+++ app/models/user_post.rb
@@ -1,6 +1,6 @@
 class UserPost < ApplicationRecord
   belongs_to :user
-  has_many :post_comments, dependent: :destroy
+  has_many :user_post_comments, dependent: :destroy
 
   validates :body, presence: true, length: { maximum: 140 }
 end
