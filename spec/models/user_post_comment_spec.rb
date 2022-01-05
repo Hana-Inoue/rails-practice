37e53558ccd1f029c1adeffa5d0c788785b90dd4
Hana-Inoue <0806han.i@gmail.com>
2022-01-05 13:57:55 +0900

    UserPostComment model spec に commented_by に関するテストを追記

--- spec/models/user_post_comment_spec.rb
+++ spec/models/user_post_comment_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe UserPostComment, type: :model do
   let(:user) { create(:user) }
 
   describe 'validation' do
-    context 'bodyが適切な値だった場合' do
+    context '適切な値が全て入力された場合' do
       it '有効になる' do
         expect(user_post_comment).to be_valid
       end
@@ -23,6 +23,20 @@ RSpec.describe UserPostComment, type: :model do
 
     context 'bodyが141文字以上の場合' do
       before { user_post_comment.body = 'a' * 141 }
+      it '無効になる' do
+        expect(user_post_comment).not_to be_valid
+      end
+    end
+
+    context 'commented_byが空文字の場合' do
+      before { user_post_comment.commented_by = '' }
+      it '無効になる' do
+        expect(user_post_comment).not_to be_valid
+      end
+    end
+
+    context 'commented_byが31文字以上の場合' do
+      before { user_post_comment.commented_by = 'a' * 31 }
       it '無効になる' do
         expect(user_post_comment).not_to be_valid
       end
