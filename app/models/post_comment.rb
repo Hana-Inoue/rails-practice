828f6fcabc8748e8bf4a62a21ed80eca89191d06
Hana-Inoue <0806han.i@gmail.com>
2022-01-05 13:34:54 +0900

    UserPostComment モデルの修正

--- app/models/post_comment.rb
+++
@@ -1,6 +0,0 @@
-class PostComment < ApplicationRecord
-  belongs_to :user
-  belongs_to :user_post
-
-  validates :body, presence: true, length: { maximum: 140 }
-end
---
+++ app/models/user_post_comment.rb
@@ -0,0 +1,7 @@
+class UserPostComment < ApplicationRecord
+  belongs_to :user
+  belongs_to :user_post
+
+  validates :body, presence: true, length: { maximum: 140 }
+  validates :commented_by, presence: true, length: { maximum: 30 }
+end
