25cfac000b728cc08692342812df02ea67af5889
Hana-Inoue <0806han.i@gmail.com>
2022-01-05 17:06:17 +0900

    user_posts index アクションを実装

---
+++ app/controllers/user_posts_controller.rb
@@ -0,0 +1,20 @@
+class UserPostsController < ApplicationController
+  def index
+    @user_posts = UserPost.all
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
