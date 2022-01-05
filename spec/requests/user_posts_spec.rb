8fd1a38a60b93db81932c4826951cd61a1b4ed22
Hana-Inoue <0806han.i@gmail.com>
2022-01-05 21:35:24 +0900

    user_posts edit アクションの request spec を作成

--- spec/requests/user_posts_spec.rb
+++ spec/requests/user_posts_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe "UserPosts", type: :request do
     log_in(user)
   end
   let(:user) { create(:user, :admin) }
+  let(:user_post) { create(:user_post, user: user) }
+
 
   describe "GET indexページ" do
     it '200番ステータスを返す' do
@@ -17,6 +19,13 @@ RSpec.describe "UserPosts", type: :request do
   describe 'GET newページ' do
     it '200番ステータスを返す' do
       get new_user_post_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET editページ' do
+    it '200番ステータスを返す' do
+      get edit_user_post_path(user_post)
       expect(response).to have_http_status(200)
     end
   end
