db471754d5d694ef6e577f67875cc204e6f96e09
Hana-Inoue <0806han.i@gmail.com>
2022-01-07 17:11:24 +0900

    Tag モデルを作成

---
+++ app/models/tag.rb
@@ -0,0 +1,6 @@
+class Tag < ApplicationRecord
+  has_many :user_posts, through: :user_post_tags, dependent: :destroy
+  has_many :user_post_tags
+
+  validates :name, presence: true, length: { maximum: 20 }
+end
