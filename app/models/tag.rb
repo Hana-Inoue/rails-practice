19d88480c56acfb859549a870c0b61efcd6563ff
Hana-Inoue <0806han.i@gmail.com>
2022-01-07 20:56:07 +0900

    Tag モデルのアソシエーションを修正

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
-  has_many :user_posts, through: :user_post_tags, dependent: :destroy
-  has_many :user_post_tags
+  has_many :user_posts, through: :user_post_tags
+  has_many :user_post_tags, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 20 }
 end
