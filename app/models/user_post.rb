d026045f18ef1536dd406dfeb7ed5df4148e76b5
Hana-Inoue <0806han.i@gmail.com>
2022-01-07 21:24:53 +0900

    アソシエーションの記述順を修正

--- app/models/user_post.rb
+++ app/models/user_post.rb
@@ -1,8 +1,8 @@
 class UserPost < ApplicationRecord
   belongs_to :user
   has_many :user_post_comments, dependent: :destroy
-  has_many :tags, through: :user_post_tags
   has_many :user_post_tags, dependent: :destroy
+  has_many :tags, through: :user_post_tags
 
   validates :body, presence: true, length: { maximum: 140 }
 end
