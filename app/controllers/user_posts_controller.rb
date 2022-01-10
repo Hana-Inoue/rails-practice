d20f38315a9127a31f4b2bff2497dcd17fbfe35f
Hana-Inoue <0806han.i@gmail.com>
2022-01-10 12:45:43 +0900

    create, update アクションを修正

--- app/controllers/user_posts_controller.rb
+++ app/controllers/user_posts_controller.rb
@@ -14,9 +14,9 @@ class UserPostsController < ApplicationController
   end
 
   def create
-    @user_post = current_user.user_posts.build(body: user_post_params[:body])
+    @user_post = current_user.user_posts.new(user_post_params)
 
-    if @user_post.save_user_post_and_tags(user_post_params[:tag_ids])
+    if @user_post.save
       redirect_to user_posts_path, notice: t('layouts.flash.messages.created_user_post')
     else
       @tags = Tag.all
@@ -27,7 +27,7 @@ class UserPostsController < ApplicationController
   def update
     @user_post = UserPost.find(params[:id])
 
-    if @user_post.update_user_post_and_tags(user_post_params)
+    if @user_post.update(user_post_params)
       redirect_to user_posts_path, notice: t('layouts.flash.messages.updated_user_post')
     else
       @tags = Tag.all
