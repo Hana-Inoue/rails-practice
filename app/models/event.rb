faa07a659410cb58e5efec16870ed8837ffdd451
Hana-Inoue <0806han.i@gmail.com>
2022-01-12 23:47:21 +0900

    日時を整形するメソッドを作成

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,12 @@ class Event < ApplicationRecord
   validates :start_at, presence: true
   validates :finish_at, presence: true
   validates :host, presence: true, length: { maximum: 30 }
+
+  def start_and_finish_datetime
+    "#{format_datetime(start_at)} ~ #{format_datetime(finish_at)}"
+  end
+
+  def format_datetime(datetime)
+    datetime.strftime("%F %H:%M")
+  end
 end
