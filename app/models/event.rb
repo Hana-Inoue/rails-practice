ac5df4a475c950fb93e8c33e025f1d2389e3abfc
Hana-Inoue <0806han.i@gmail.com>
2022-01-14 12:38:28 +0900

    開始日が終了日よりも未来だった場合のバリデーションを設定

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,11 +6,19 @@ class Event < ApplicationRecord
   validates :finish_at, presence: true
   validates :host, presence: true, length: { maximum: 30 }
 
+  validate :finish_at_after_start_at
+
   def start_and_finish_datetime
     "#{format_datetime(start_at)} ~ #{format_datetime(finish_at)}"
   end
 
   def format_datetime(datetime)
     datetime.strftime("%F %H:%M")
+  end
+
+  private
+
+  def finish_at_after_start_at
+    errors.add(:finish_at, :invalid) unless start_at < finish_at
   end
 end
