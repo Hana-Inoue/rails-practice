35694cfe2fde714169009a2e10755859401b6b7f
Hana-Inoue <0806han.i@gmail.com>
2022-01-18 23:35:35 +0900

    scope を修正

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -5,10 +5,8 @@ class Todo < ApplicationRecord
     due_date_is(todos_search_params[:due_date])
       .completed_date_is(todos_search_params[:completed_date])
   end
-  scope :due_date_is, -> (due_date) do
-    where('due_date = ?', due_date) if due_date.present?
-  end
+  scope :due_date_is, -> (due_date) { where(due_date: due_date) if due_date.present? }
   scope :completed_date_is, -> (completed_date) do
-    where('completed_date = ?', completed_date) if completed_date.present?
+    where(completed_date: completed_date) if completed_date.present?
   end
 end
