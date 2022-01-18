78aaab1d76d5503fe2c21d003dadb11f6a25fe95
Hana-Inoue <0806han.i@gmail.com>
2022-01-18 22:09:33 +0900

    todos search アクションを実装

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,7 +1,23 @@
 class TodosController < ApplicationController
   def index
+    @todos_search_params = todos_search_params
     @pages, @todos = paginate(active_record: Todo.order(:id),
                               previous_and_next_page_count: 2,
                               max_item_count: 20)
+  end
+
+  def search
+    @todos_search_params = todos_search_params
+    @pages, @todos = paginate(active_record: Todo.search(@todos_search_params).order(:id))
+
+    render :index
+  end
+
+  private
+
+  def todos_search_params
+    params
+      .fetch(:search, {})
+      .permit(:due_date, :completed_date)
   end
 end
