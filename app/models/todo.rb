1a55e9e9c38577ef7f84ad3b81a03c8101664863
Hana-Inoue <0806han.i@gmail.com>
2022-01-18 22:10:00 +0900

    Todo モデルに scope を作成

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,14 @@
 class Todo < ApplicationRecord
+  scope :search, -> (todos_search_params) do
+    return if todos_search_params.blank?
+
+    due_date_is(todos_search_params[:due_date])
+      .completed_date_is(todos_search_params[:completed_date])
+  end
+  scope :due_date_is, -> (due_date) do
+    where('due_date = ?', due_date) if due_date.present?
+  end
+  scope :completed_date_is, -> (completed_date) do
+    where('completed_date = ?', completed_date) if completed_date.present?
+  end
 end
