edb8de58faa8395d691bbbeb9560f7bfce353b61
Hana-Inoue <0806han.i@gmail.com>
2022-01-18 23:13:29 +0900

    検索速度を出力

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -7,8 +7,10 @@ class TodosController < ApplicationController
   end
 
   def search
+    start_time = Time.now
     @todos_search_params = todos_search_params
     @pages, @todos = paginate(active_record: Todo.search(@todos_search_params).order(:id))
+    @search_time = (Time.now - start_time).round(3)
 
     render :index
   end
