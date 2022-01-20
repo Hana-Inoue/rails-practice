799f03d85c4fac9b8025974867bb0f3de137acfb
Hana-Inoue <0806han.i@gmail.com>
2022-01-20 14:43:54 +0900

    UserDiary モデルにバリデーションを設定

--- app/models/user_diary.rb
+++ app/models/user_diary.rb
@@ -1,3 +1,6 @@
 class UserDiary < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :body, presence: true, length: { maximum: 300 }
 end
