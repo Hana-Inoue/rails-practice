10ac6672117d0fbac3a33a72dd4ac8e2ba2b1556
Hana-Inoue <0806han.i@gmail.com>
2022-01-21 12:41:24 +0900

    アクセス権限のチェック方法を修正

--- app/controllers/user_diaries_controller.rb
+++ app/controllers/user_diaries_controller.rb
@@ -1,23 +1,20 @@
 class UserDiariesController < ApplicationController
+  before_action :check_authorization_with_user_id
+  before_action :check_authorization_with_user_diary_id, only: [:show, :edit, :update, :destroy]
+
   def index
     @user = User.find(params[:user_id])
     @pages, @user_diaries = paginate(active_record: @user.user_diaries.order(:id))
-
-    @user_diaries.each { |user_diary| check_authorization_for(user_diary) }
   end
 
   def show
     @user = User.find(params[:user_id])
     @user_diary = UserDiary.find(params[:id])
-
-    check_authorization_for(@user_diary)
   end
 
   def new
     @user = User.find(params[:user_id])
     @user_diary = @user.user_diaries.build
-
-    check_authorization_for(@user_diary)
   end
 
   def edit
@@ -26,8 +23,6 @@ class UserDiariesController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     @user_diary = @user.user_diaries.build(user_diary_params)
-
-    check_authorization_for(@user_diary)
 
     if @user_diary.save
       redirect_to user_user_diary_path(@user, @user_diary),
@@ -45,8 +40,12 @@ class UserDiariesController < ApplicationController
 
   private
 
-  def check_authorization_for(user_diary)
-    raise NotAuthorizedError unless current_user.id == user_diary.user_id
+  def check_authorization_with_user_id
+    raise NotAuthorizedError unless current_user.id == params[:user_id].to_i
+  end
+
+  def check_authorization_with_user_diary_id
+    raise NotAuthorizedError unless current_user.id == UserDiary.find(params[:id]).user_id
   end
 
   def user_diary_params
