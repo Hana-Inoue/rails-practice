fccc778121c2eb40047c157869c0dc57b3f4d820
Hana-Inoue <0806han.i@gmail.com>
2022-01-24 20:40:14 +0900

    show アクションを修正

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,13 +1,10 @@
 class SchedulesController < ApplicationController
-  # SQL Injection が発生しないアクション
   def index
     @pages, @schedules = paginate(active_record: Schedule.all)
   end
 
-  # SQL Injection が発生するアクション
-  # url :id 部分に 1) OR 1=1-- と入力することで全スケジュール詳細を表示
   def show
-    @schedules = Schedule.where("id = #{params[:id]}")
+    @schedule = Schedule.find(params[:id])
   end
 
   def new
