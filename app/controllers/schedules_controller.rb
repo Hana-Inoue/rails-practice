e1e9b9746daef1a54a4e3c06d714d8cab4a717a8
Hana-Inoue <0806han.i@gmail.com>
2022-01-24 20:30:41 +0900

    create アクションを実装

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -10,7 +10,6 @@ class SchedulesController < ApplicationController
     @schedules = Schedule.where("id = #{params[:id]}")
   end
 
-  # SQL Injection が発生しないアクション
   def new
     @schedule = Schedule.new
   end
@@ -19,11 +18,26 @@ class SchedulesController < ApplicationController
   end
 
   def create
+    @schedule = Schedule.new(schedule_params)
+
+    if @schedule.save
+      redirect_to @schedule, notice: t('layouts.flash.messages.created_schedule')
+    else
+      render :new
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def schedule_params
+    params
+      .require(:schedule)
+      .permit(:name, :scheduled_for, :scheduled_by)
   end
 end
