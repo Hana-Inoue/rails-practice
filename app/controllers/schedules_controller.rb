2c92427b8f49a9b378ab44b94443dbe49a3ffff0
Hana-Inoue <0806han.i@gmail.com>
2022-01-24 16:32:05 +0900

    show アクションを実装

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -4,7 +4,10 @@ class SchedulesController < ApplicationController
     @pages, @schedules = paginate(active_record: Schedule.all)
   end
 
+  # SQL Injection が発生するアクション
+  # url :id 部分に 1) OR 1=1-- と入力することで全スケジュール詳細を表示
   def show
+    @schedules = Schedule.where("id = #{params[:id]}")
   end
 
   def new
