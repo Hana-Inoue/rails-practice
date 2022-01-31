b84d19cab1d69f30c4b08dd17b6ecab8ff2de08f
Hana-Inoue <0806han.i@gmail.com>
2022-01-31 11:51:24 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  get 'static_pages/about_server_logs'
   get 'static_pages/about_activerecord_logs'
+  get 'static_pages/about_server_logs'
+  get 'static_pages/search_functions_summary'
 
   resources :users do
     resource :authorizations, only: [:edit, :update]
