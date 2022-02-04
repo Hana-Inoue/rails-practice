0baa3ab9b91c02e165ec71eef9c0c6086fa2e887
Hana-Inoue <0806han.i@gmail.com>
2022-02-05 01:04:50 +0900

    carts destroy アクションを実装

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -8,10 +8,14 @@ class CartsController < ApplicationController
     session[:cart] ||= []
     session[:cart].unshift(params[:product_id])
     redirect_to products_path,
-                notice: t('layouts.flash.messages.added_to_cart',
+                notice: t('layouts.flash.messages.added_product_to_cart',
                           product: Product.find(params[:product_id]).name)
   end
 
   def destroy
+    product = Product.find(session[:cart][params[:product_index].to_i])
+    session[:cart].delete_at(params[:product_index].to_i)
+    redirect_to cart_path,
+                notice: t('layouts.flash.messages.deleted_product_from_cart', product: product.name)
   end
 end
