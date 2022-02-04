c89bb5b24b3f867802274c61e6d6341c6bf0ece5
Hana-Inoue <0806han.i@gmail.com>
2022-02-04 14:50:16 +0900

    Product モデルを作成

---
+++ app/models/product.rb
@@ -0,0 +1,4 @@
+class Product < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
+end
