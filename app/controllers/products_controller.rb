b72ba9268809c3b2584c30dffa87b40bb759ea81
Hana-Inoue <0806han.i@gmail.com>
2022-02-04 17:40:56 +0900

    add_product_to_cart アクションを実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,12 @@ class ProductsController < ApplicationController
   def index
     @pages, @products = paginate(active_record: Product.all)
   end
+
+  def add_product_to_cart
+    session[:cart] ||= []
+    session[:cart] << params[:id]
+    redirect_to products_path,
+                notice: t('layouts.flash.messages.added_to_cart',
+                        product: Product.find(params[:id]).name)
+  end
 end
