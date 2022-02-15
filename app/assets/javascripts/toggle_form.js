ae8f4951483c76704255f5a6b2be5896d19de6c3
Hana-Inoue <0806han.i@gmail.com>
2022-02-15 22:43:10 +0900

    ボタンの文言を出し分ける処理を修正

--- app/assets/javascripts/toggle_form.js
+++ app/assets/javascripts/toggle_form.js
@@ -1,11 +1,12 @@
 $(function() {
   $('.js-toggle-form-target').hide();
+  var buttonText = $('.js-toggle-form-button').text();
   $('.js-toggle-form-button').click(function() {
     $('.js-toggle-form-target').slideToggle(1000);
-    if ($(this).text() === '検索') {
+    if ($(this).text() === buttonText) {
       $(this).text('閉じる');
     } else {
-      $(this).text('検索');
+      $(this).text(buttonText);
     }
   });
 });
