29d29af521d605c52e9dd433226b5b009bb74f83
Hana-Inoue <0806han.i@gmail.com>
2022-02-16 14:39:59 +0900

    条件分岐を修正

--- app/assets/javascripts/toggle_form.js
+++ app/assets/javascripts/toggle_form.js
@@ -1,12 +1,11 @@
 $(document).on('turbolinks:load', function() {
   $('.js-toggle-form-target').hide();
+
   let buttonText = $('.js-toggle-form-button').text();
+
   $('.js-toggle-form-button').click(function() {
     $('.js-toggle-form-target').slideToggle(1000);
-    if ($(this).text() === buttonText) {
-      $(this).text('閉じる');
-    } else {
-      $(this).text(buttonText);
-    }
+    if ($(this).text() === buttonText) return $(this).text('閉じる');
+    $(this).text(buttonText);
   });
 });
