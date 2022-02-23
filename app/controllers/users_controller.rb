77b93b048a85238e75c38c2fa99252715b10817e
Hana-Inoue <0806han.i@gmail.com>
2022-02-23 21:37:51 +0900

    users コントローラに before_action を設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,19 @@
 class UsersController < ApplicationController
+  before_action :user, only: [:show, :edit, :update]
+
   def index
     @pages, @users = paginate(active_record: User.order(:id),
                               previous_and_next_page_count: 3,
                               max_item_count: 10)
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
+  def show; end
 
   def new
     @user = User.new
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def create
     @user = User.new(user_params)
@@ -28,8 +26,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
-
     if @user.update(user_params)
       redirect_to @user
     else
@@ -38,13 +34,21 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    user = User.find(params[:id])
+    user = find_user
 
     user.destroy
     redirect_to root_path, notice: t('layouts.flash.messages.deleted_user', name: user.name)
   end
 
   private
+
+  def user
+    @user ||= find_user
+  end
+
+  def find_user
+    User.find(params[:id])
+  end
 
   def user_params
     params
