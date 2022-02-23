feac39b56bded6bee1aa607bc3553aa92d1f53f0
Hana-Inoue <0806han.i@gmail.com>
2022-02-23 22:18:37 +0900

    users request spec をリファクタリング

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  let(:user) { create(:user, :admin) }
   before do
     create_authorizations
     log_in(user)
   end
-  let(:user) { create(:user, :admin) }
-  let(:other_user) { create(:user) }
 
   describe 'GET indexページ' do
     it '200番ステータスを返す' do
@@ -17,7 +16,7 @@ RSpec.describe 'Users', type: :request do
 
   describe 'GET showページ' do
     it '200番ステータスを返す' do
-      get user_path(other_user)
+      get user_path(user)
       expect(response).to have_http_status(200)
     end
   end
@@ -31,7 +30,7 @@ RSpec.describe 'Users', type: :request do
 
   describe 'GET editページ' do
     it '200番ステータスを返す' do
-      get edit_user_path(other_user)
+      get edit_user_path(user)
       expect(response).to have_http_status(200)
     end
   end
@@ -54,31 +53,38 @@ RSpec.describe 'Users', type: :request do
     end
 
     context '無効なリクエストパラメータが渡された場合' do
+      before { user_params[:name] = '' }
+
       it '200番ステータスを返す' do
-        user_params[:name] = ''
         post users_path, params: { user: user_params }
         expect(response).to have_http_status(200)
+      end
+
+      it 'Userが増えない' do
+        expect {
+          post users_path, params: { user: user_params }
+        }.to change(User, :count).by(0)
       end
     end
   end
 
   describe 'PATCH User情報' do
-    before { user_params[:gender] = gender }
     let(:user_params) { attributes_for(:user) }
+    before { user_params[:gender] = gender }
 
     context '有効なリクエストパラメータが渡された場合' do
       let(:gender) { :women }
 
       it 'showページへリダイレクトする' do
-        patch user_path(other_user), params: { user: user_params }
+        patch user_path(user), params: { user: user_params }
         expect(response).to have_http_status(302)
-        expect(response).to redirect_to user_path(User.last)
+        expect(response).to redirect_to user
       end
 
       it '任意のUserの値を変更する' do
         expect {
-          patch user_path(other_user), params: { user: user_params }
-        }.to change { User.last[:gender] }.from('men').to('women')
+          patch user_path(user), params: { user: user_params }
+        }.to change { user.reload.gender }.from('men').to('women')
       end
     end
 
@@ -86,13 +92,14 @@ RSpec.describe 'Users', type: :request do
       let(:gender) { '' }
 
       it '200番ステータスを返す' do
-        patch user_path(other_user), params: { user: user_params }
+        patch user_path(user), params: { user: user_params }
         expect(response).to have_http_status(200)
       end
     end
   end
 
   describe 'DELETE User情報' do
+    let(:other_user) { create(:user) }
     before { other_user }
 
     it 'indexページへリダイレクトする' do
