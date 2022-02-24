b3c3d448099de532ceced7b759a1fb2993ad4727
Hana-Inoue <0806han.i@gmail.com>
2022-02-24 20:26:34 +0900

    find_user を削除

--- app/controllers/user_addresses_controller.rb
+++ app/controllers/user_addresses_controller.rb
@@ -1,11 +1,11 @@
 class UserAddressesController < ApplicationController
   def edit
-    @user = find_user
+    @user = User.find(params[:user_id])
     @user_address = @user.user_address || @user.build_user_address
   end
 
   def update
-    @user = find_user
+    @user = User.find(params[:user_id])
     @user_address = @user.user_address || @user.build_user_address
 
     if @user_address.update(user_address_params)
@@ -16,17 +16,13 @@ class UserAddressesController < ApplicationController
   end
 
   def destroy
-    user = find_user
+    user = User.find(params[:user_id])
 
     user.user_address.destroy
     redirect_to user, notice: t('layouts.flash.messages.deleted_address')
   end
 
   private
-
-  def find_user
-    User.find(params[:user_id])
-  end
 
   def user_address_params
     params
