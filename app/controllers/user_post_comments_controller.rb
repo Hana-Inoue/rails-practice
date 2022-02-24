598ad1204bab113542c57e94679ada2976cfb36d
Hana-Inoue <0806han.i@gmail.com>
2022-02-24 16:36:52 +0900

    if 式をリファクタリング

--- app/controllers/user_post_comments_controller.rb
+++ app/controllers/user_post_comments_controller.rb
@@ -11,14 +11,15 @@ class UserPostCommentsController < ApplicationController
     @user_post_comment = @user_post.user_post_comments.build(user_post_comment_params)
 
     if @user_post_comment.save
-      redirect_to user_post_user_post_comments_path(@user_post),
-                  notice: t('layouts.flash.messages.created_user_post_comment')
-    else
-      @pages, @user_post_comments =
-        paginate(
-          active_record: UserPostComment.where(user_post_id: params[:user_post_id]).order(:id))
-      render :index
+      redirect_to(
+        user_post_user_post_comments_path(@user_post),
+        notice: t('layouts.flash.messages.created_user_post_comment')
+      ) and return
     end
+    @pages, @user_post_comments =
+      paginate(
+        active_record: UserPostComment.where(user_post_id: params[:user_post_id]).order(:id))
+    render :index
   end
 
   def destroy
