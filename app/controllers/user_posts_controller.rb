aea486c2c8c6d12bf17a507294a11fca2d125380
Hana-Inoue <0806han.i@gmail.com>
2022-02-24 20:38:24 +0900

    インスタンス変数をセットするメソッドを削除

--- app/controllers/user_posts_controller.rb
+++ app/controllers/user_posts_controller.rb
@@ -1,6 +1,4 @@
 class UserPostsController < ApplicationController
-  before_action :user_post, only: [:edit, :update]
-  before_action :tags, only: [:new, :edit]
 
   def index
     @pages, @user_posts = paginate(active_record: UserPost.order(:id),
@@ -9,9 +7,13 @@ class UserPostsController < ApplicationController
 
   def new
     @user_post = UserPost.new
+    @tags = Tag.all
   end
 
-  def edit; end
+  def edit
+    @user_post = UserPost.find(params[:id])
+    @tags = Tag.all
+  end
 
   def create
     @user_post = current_user.user_posts.build(user_post_params)
@@ -19,36 +21,26 @@ class UserPostsController < ApplicationController
     if @user_post.save
       redirect_to user_posts_path, notice: t('layouts.flash.messages.created_user_post') and return
     end
-    tags
+    @tags = Tag.all
     render :new
   end
 
   def update
+    @user_post = UserPost.find(params[:id])
+
     if @user_post.update(user_post_params)
       redirect_to user_posts_path, notice: t('layouts.flash.messages.updated_user_post') and return
     end
-    tags
+    @tags = Tag.all
     render :edit
   end
 
   def destroy
-    find_user_post.destroy
+    UserPost.find(params[:id]).destroy
     redirect_to user_posts_path, notice: t('layouts.flash.messages.deleted_user_post')
   end
 
   private
-
-  def tags
-    @tags ||= Tag.all
-  end
-
-  def user_post
-    @user_post ||= find_user_post
-  end
-
-  def find_user_post
-    UserPost.find(params[:id])
-  end
 
   def user_post_params
     params.require(:user_post).permit(:body, tag_ids: [])
