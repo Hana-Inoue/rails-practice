ed5a59bb153f2596c7b550bb693a539ecd147a2a
Hana-Inoue <0806han.i@gmail.com>
2022-03-07 12:19:36 +0900

    Revert "find_user_post を削除"
    This reverts commit fd82d78fc1a7966e44084fd344982f915364991a.

--- app/controllers/user_post_comments_controller.rb
+++ app/controllers/user_post_comments_controller.rb
@@ -1,12 +1,12 @@
 class UserPostCommentsController < ApplicationController
   def index
-    @user_post = UserPost.find(params[:user_post_id])
+    @user_post = find_user_post
     @user_post_comment = @user_post.user_post_comments.build
     @pages, @user_post_comments = paginate(active_record: @user_post.user_post_comments.order(:id))
   end
 
   def create
-    @user_post = UserPost.find(params[:user_post_id])
+    @user_post = find_user_post
     @user_post_comment = @user_post.user_post_comments.build(user_post_comment_params)
 
     if @user_post_comment.save
@@ -21,11 +21,15 @@ class UserPostCommentsController < ApplicationController
 
   def destroy
     UserPostComment.find(params[:id]).destroy
-    redirect_to user_post_user_post_comments_path(UserPost.find(params[:user_post_id])),
+    redirect_to user_post_user_post_comments_path(find_user_post),
                 notice: t('layouts.flash.messages.deleted_user_post_comment')
   end
 
   private
+
+  def find_user_post
+    UserPost.find(params[:user_post_id])
+  end
 
   def user_post_comment_params
     params.require(:user_post_comment).permit(:body).merge(commented_by: current_user.name)
