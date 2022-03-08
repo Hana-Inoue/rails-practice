afc8ea2b84dc9f3f78849b5dc6c4483b2aa6bad7
Hana-Inoue <0806han.i@gmail.com>
2022-03-08 11:16:14 +0900

    sessions に関連する文言を移植

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,15 +9,15 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:session][:password])
       log_in(user)
-      redirect_to root_path, notice: t('layouts.flash.messages.log_in.success')
+      redirect_to root_path, notice: t('sessions.flash.messages.log_in.success')
     else
-      flash.now[:alert] = t('layouts.flash.messages.log_in.fail')
+      flash.now[:alert] = t('sessions.flash.messages.log_in.fail')
       render :new
     end
   end
 
   def destroy
     log_out
-    redirect_to login_path, notice: t('layouts.flash.messages.log_out')
+    redirect_to login_path, notice: t('sessions.flash.messages.log_out')
   end
 end
